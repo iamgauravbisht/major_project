c0e62ea5355e22f6b9bf0c64d8557df43297e794
Gaurav <gauravbisht154@gmail.com>
2024-04-12 10:48:58 +0530

    updated backend url

--- src/lib/call.js
+++ src/lib/call.js
@@ -1,14 +1,20 @@
 export const predict = async (data) => {
-  return await fetch("http://127.0.0.1:8000/predict", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => res.json());
+  return await fetch(
+    "https://major-project-backend-ooal.onrender.com/predict",
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  ).then((res) => res.json());
 };
 export const performance = async () => {
-  return await fetch("http://127.0.0.1:8000/performance", {
-    method: "GET",
-  });
+  return await fetch(
+    "https://major-project-backend-ooal.onrender.com/performance",
+    {
+      method: "GET",
+    }
+  );
 };
